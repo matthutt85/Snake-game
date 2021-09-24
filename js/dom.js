e1f50f3cab46de86af9c6fe607b64d90299fbcb9
matthutt85 <matt.hutt85@gmail.com>
2021-09-24 12:46:54 +0100

    selectors designed

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,3 @@
 export const grid = document.querySelector(".grid");
+export const player1 = document.querySelector(".selectors__player1")
+export const player2 = document.querySelector(".selectors__player2")
